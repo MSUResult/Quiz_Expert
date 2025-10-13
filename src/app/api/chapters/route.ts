e4145bca46a714c39e60ec3edefad9565a1bc324
MSUResult <adarshrajput9040@gmail.com>
2025-10-13 17:46:32 +0530

    her thise is

--- src/app/api/chapters/route.ts
+++ src/app/api/chapters/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import dbConnect from "../../../../lib/db";
+import dbConnect from "@/lib/db";
 import Chapter from "@/models/chapters";
 
 export async function POST(request) {
